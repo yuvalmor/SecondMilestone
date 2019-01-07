280ed6e4aa5f434d51d65d17f24e8d1c418e8605
yuvalmor <yuvalmor94@gmail.com>
2019-01-07 19:50:18 +0200

    open new classes

---
+++ MySerialServer.cpp
@@ -0,0 +1,9 @@
+#include "MySerialServer.h"
+
+void MySerialServer::open(int port) {
+
+}
+
+void MySerialServer::stop() {
+
+}
---
+++ MySerialServer.h
@@ -0,0 +1,13 @@
+#ifndef SECONDMILESTONE_MYSERIALSERVER_H
+#define SECONDMILESTONE_MYSERIALSERVER_H
+
+#include "Server_side.h"
+
+using namespace server_side;
+
+class MySerialServer:public Server{
+
+};
+
+#endif
+
