da882b05d23f76275acda8dc277159b0ac970be8
yuvalmor <yuvalmor94@gmail.com>
2019-01-09 19:53:07 +0200

    edit clientHandler

--- ClientHandler.h
+++ ClientHandler.h
@@ -1,8 +1,12 @@
 #ifndef SECONDMILESTONE_CLIENTHANDLER_H
 #define SECONDMILESTONE_CLIENTHANDLER_H
 
+/**
+ * The interface ClientHandler represent the specific way of communication with the client
+ */
 class ClientHandler{
-
+public:
+    virtual void handleClient(int socket) = 0;
 };
 
 #endif //SECONDMILESTONE_CLIENTHANDLER_H
