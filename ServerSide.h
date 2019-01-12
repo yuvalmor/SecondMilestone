2666e2ce5fe668c086663c2ba65c66475a6a1540
yuvalmor <yuvalmor94@gmail.com>
2019-01-12 12:17:58 +0200

    yuval update version

--- ServerSide.h
+++ ServerSide.h
@@ -8,12 +8,17 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include "ClientHandler.h"
 
 namespace server_side{
     class Server{
     public:
-        virtual void open(int port)=0;
+        virtual void open(int portNum,ClientHandler* clientHandler)=0;
         virtual void stop()=0;
+        void Error(const char* errorMassage){
+            perror(errorMassage);
+            exit(1);
+        }
     };
 }
 
--- main.cpp
+++ main.cpp
@@ -1,4 +1,21 @@
-//
-// Created by yuval on 1/12/19.
-//
+#define NO_PARAMETERS_TO_MAIN 2
 
+#include <iostream>
+#include "MySerialServer.h"
+#include "ClientHandler.h"
+#include "MyClientHandler.h"
+
+using namespace server_side;
+
+int main(int argc,char* argv[]) {
+    if(argc<NO_PARAMETERS_TO_MAIN){
+        cout << "Error - no port provided" <<endl;
+        exit(1);
+    }
+    ClientHandler* clientHandler = new MyClientHandler();
+    Server* server = new MySerialServer();
+    int portNumber = atoi(argv[1]);
+    server->open(portNumber,clientHandler);
+    cout<<"end of program"<<endl;
+    return 0;
+}
