71095e3b92386a29f8831b3071c10629e602a966
yuvalmor <yuvalmor94@gmail.com>
2019-01-16 17:52:44 +0200

    Merge branch 'master' of https://github.com/yuvalmor/SecondMilestone
    # Conflicts:
    #	CMakeLists.txt
    #	MyClientHandler.cpp
    #	MyClientHandler.h
    #	ServerSide.h
    #	main.cpp

--- MySolver.h
+++ MySolver.h
@@ -1,26 +1,8 @@
+//
+// Created by yuval on 1/13/19.
+//
+
 #ifndef SECONDMILESTONE_MYSOLVER_H
 #define SECONDMILESTONE_MYSOLVER_H
 
-#include "Solver.h"
-#include "Solution.h"
-#include "Searcher.h"
-
-/**
- * MySolver - solve search problems,
- * Has the algorithm as data member, and use it to solve the problem.
- */
-class MySolver:public Solver{
-    Searcher<vector<int>>* searcher;
-public:
-    // Constructor
-    MySolver( Searcher<vector<int>>* algorithm){
-        this->searcher = algorithm;
-    }
-    // Solve the problem using the method search
-    Solution<vector<int>> solve(Searchable<vector<int>>* problem){
-        Solution<vector<int>> solution = this->searcher->search(problem);
-        return solution;
-    }
-};
-
-#endif
+#endif //SECONDMILESTONE_MYSOLVER_H
--- Solver.h
+++ Solver.h
@@ -1,16 +1,8 @@
+//
+// Created by yuval on 1/13/19.
+//
+
 #ifndef SECONDMILESTONE_SOLVER_H
 #define SECONDMILESTONE_SOLVER_H
 
-#include "Solution.h"
-#include "Searchable.h"
-
-/**
- * Interface solver - responsible to solve given problems
- */
-class Solver{
-public:
-    // Solve the given problem and return its solution
-    virtual Solution<vector<int>> solve(Searchable<vector<int>>* problem)=0;
-};
-
-#endif
+#endif //SECONDMILESTONE_SOLVER_H
